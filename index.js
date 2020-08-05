a72d5a9c339c346d3567cde6b4c2ed63d627be9f
AlveeM <alvee.akand@outlook.com>
2020-08-04 21:58:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+  let formData = {
+    name: userName,
+    email: userEmail
+  };
+
+  let configObj = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+
+  return fetch('http://localhost:3000/users', configObj)
+    .then(resp => resp.json())
+    .then(obj => document.body.innerHTML = obj.id)
+    .catch(err => document.body.innerHTML = err.message)
+}
