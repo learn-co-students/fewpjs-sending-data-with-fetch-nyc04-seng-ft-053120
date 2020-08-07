09f5c1d7062815f32c93ca42acf82449e185b55c
mariamoljames <mariamol.james@gmail.com>
2020-08-07 00:12:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          name,
+          email
+        })
+      })
+        .then(r => r.json())
+        .then((newUser) => {
+          document.body.innerHTML = newUser["id"]
+        })
+        .catch((error) => {
+            document.body.innerHTML = error.message
+        })
+}
+
