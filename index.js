900253fde6a1a81adcd37bbc6d7d96dc8ea0ebc2
21shield <21netaly@gmail.com>
2020-08-06 23:01:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+
+function submitData (usersName, usersEmail){
+   return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(
+            {
+                name: usersName,
+                email: usersEmail
+            }
+        )
+    })
+    .then(response => (response.json()))
+    .then(userInfo => {
+        document.body.append(userInfo.id)
+        
+    }).catch(error =>{
+        document.body.append(error.message);
+    })
+}
+
+  submitData("21shield", "email@.com")
+//  submitData("21shield", "email@.com")
+//  submitData("21shield", "email@.com")
