965614c9757bac93b19b4bc1eecb71d8163f4726
Ginger-Mano <mcasimirian@gmail.com>
2020-08-09 14:34:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+return fetch(`http://localhost:3000/users`, { 
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json" //optional but lab required this
+    },
+    body: JSON.stringify({
+        name, //call what was passed in function
+        email
+    })
+})
+    .then(function (response)  {
+        return response.json()
+    })
+    .then(function(object) {
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message //handles the unexpected results
+    })
+}
