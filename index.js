5b306dcb3b5370fe2cbd16b528321fde21512802
Fraciscv <Francisco.Andaur@icloud.com>
2020-08-09 22:36:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(usersName, usersEmail) {
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: usersName,
+            email: usersEmail
+        })
+    })
+    .then(res => res.json())
+    .then(function(userObj) {
+        document.body.append(userObj.id)
+    })
+    .catch(function(error) {
+        document.body.append(error.message);
+    })
+}
