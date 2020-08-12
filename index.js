ef7ce93f50a6eee6fb16b19845a4aa878c091fea
tracycollins <tc@threeceemedia.com>
2020-08-12 12:25:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+const body = document.querySelector("body")
+
+submitData = (name, email) => {
+
+  const formData = { name: name, email: email }
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+
+  return fetch("http://localhost:3000/users", options)
+    .then(res => res.json())
+    .then(newUser => {
+
+      const userIdP = document.createElement("p")
+      userIdP.innerHTML = newUser.id
+      body.append(userIdP)
+    })
+    .catch(err => {
+      const errorP = document.createElement("p")
+      errorP.innerHTML = err.message
+      body.append(errorP)
+    })
+
+
+}
