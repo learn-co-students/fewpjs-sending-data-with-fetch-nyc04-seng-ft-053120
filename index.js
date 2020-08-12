f7d99f5858a59bbe4285e14fe20432b0b326deff
impactlearner <impactlearner2020@gmail.com>
2020-08-12 14:38:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+    function submitData(userName, userEmail){
+      let submitData = {
+        userName: userName,
+        userEmail: userEmail
+    };
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(submitData)
+      };
+       
+      return fetch("http://localhost:3000/dogs", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          console.log(object);
+        })
+        .catch(function(error) {
+          alert("Bad things! Ragnarők!");
+          console.log(error.message);
+        });
+    }
